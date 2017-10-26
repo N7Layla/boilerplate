aed3760f2b0f9017da51dc2d22b54c774e9c1a0f
Layla Hedges <layla.hedges@gmail.com>
2017-10-25 20:34:25 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(8080);
+app.use(express.static(path.join(__dirname, './public')))
+
+// Any routes or other various middlewares should go here!
+
+// Make sure this is right at the end of your server logic!
+// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// (However, if you have middleware to serve up 404s, that go would before this as well)
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
